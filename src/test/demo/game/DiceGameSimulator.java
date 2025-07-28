0a98b858c99d2c1a6ff9b2f58e51a2374bc2fc9f
Vic <Vic@DESKTOP-P7GNVOR.home>
2025-07-28 18:32:04 -0400

    First commit.

---
+++ src/test/demo/game/DiceGameSimulator.java
@@ -0,0 +1,37 @@
+package test.demo.game;
+
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public final class DiceGameSimulator {
+	
+	private static int numberOfDice = 5;
+    private static int diceSides = 6;
+    private static int numberOfSimulations = 100;
+
+    public static void main(String[] args) {
+    	
+    	Random random = new Random();
+        long start = System.nanoTime();
+        
+        int[] frequencyNums = new int[numberOfDice * diceSides + 1];
+        
+        IntStream.range(0, numberOfSimulations).forEach(i -> {
+            int total = IntStream.range(0, numberOfDice)
+                                 .map(j -> random.nextInt(diceSides) + 1)
+                                 .sum();
+            frequencyNums[total]++;
+        });
+
+        long endTime = System.nanoTime();
+
+        for (int total = numberOfDice; total <= numberOfDice * diceSides; total++) {
+            int count = frequencyNums[total];
+            double estProbability = (double) count / numberOfSimulations;
+            System.out.printf("Total %d occurs %.2f occurred %.1f times.%n", total, estProbability, (double) count);
+        }
+
+        double duration = (endTime - start) / 1_000_000_000.0;
+        System.out.printf("Total simulation took %.2f seconds.%n", duration);
+    }
+}
